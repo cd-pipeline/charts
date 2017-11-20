aa21b2cb6cbfbdf0c6f186d3405c9259d2818da6
akalinovski <a.kalinovski@gmail.com>
2017-11-20 17:51:16 +0300

    Changed pipeline configuration

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -1,3 +1,6 @@
+kubectl delete namespace cd-pipeline || true
+kubectl create namespace cd-pipeline
+
 helm delete --purge jenkins || true
 helm install --name jenkins -f ./config/jenkins.yml stable/jenkins --namespace cd-pipeline
 kubectl create secret generic jenkins-maven-settings --from-file=./settings.xml -n cd-pipeline
