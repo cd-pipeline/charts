df1959846527fe69845efb05c2ce8f61abfbf55f
akalinovski <a.kalinovski@gmail.com>
2017-11-22 17:54:41 +0300

    Changed pipeline configuration

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -7,3 +7,5 @@ sleep 10
 
 helm install --name jenkins -f ./config/jenkins.yml stable/jenkins --namespace cd-pipeline
 kubectl create secret generic jenkins-maven-settings --from-file=./settings.xml -n cd-pipeline
+
+kubectl create secret docker-registry regsecret --docker-server=quay.io/cd_pipeline/cloud-repository --docker-username=cd-pipeline --docker-password=$forsythe17@! --docker-email=akalinovski@forsythe.com -n cd-pipeline
