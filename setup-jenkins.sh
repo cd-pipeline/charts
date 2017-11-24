e7bcb2ad9ba1876b6625aece7b1369d9571bcbd4
akalinovski <a.kalinovski@gmail.com>
2017-11-24 09:12:40 +0300

    Added initial version of keycloak support

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -9,3 +9,6 @@ helm install --name jenkins -f ./config/jenkins.yml stable/jenkins --namespace c
 kubectl create secret generic jenkins-maven-settings --from-file=./settings.xml -n cd-pipeline
 
 kubectl create secret docker-registry regsecret --docker-server=quay.io/cd_pipeline/cloud-repository --docker-username=cd-pipeline --docker-password=$forsythe17@! --docker-email=akalinovski@forsythe.com -n cd-pipeline
+
+helm delete --purge keycloak || true
+helm install --name keycloak ./keycloak -f ./config/keycloak.yml --namespace cd-pipeline
