259d106515c87989f74a57004ec02e6484910a64
akalinovski <a.kalinovski@gmail.com>
2017-11-29 17:06:35 +0300

    Changed jenkins configuration

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -2,8 +2,8 @@ kubectl delete namespace devops || true
 sleep 10
 kubectl create namespace devops
 
-helm delete --purge devops_jenkins || true
+helm delete --purge devopsjenkins || true
 sleep 10
 
-helm install --name devops_jenkins -f ./jenkins.yml stable/jenkins --namespace devops
+helm install --name devopsjenkins -f ./jenkins.yml stable/jenkins --namespace devops
 kubectl create secret generic jenkins-maven-settings --from-file=./settings.xml -n devops
