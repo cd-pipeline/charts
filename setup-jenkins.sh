c0d5c1dc9b2f1b310c4f49d00d2879c0265ff68b
akalinovski <a.kalinovski@gmail.com>
2017-11-29 16:50:21 +0300

    Added initial version of keycloak support

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -1,15 +1,9 @@
-kubectl delete namespace cd-pipeline || true
+kubectl delete namespace devops || true
 sleep 10
-kubectl create namespace cd-pipeline
+kubectl create namespace devops
 
-kubectl delete namespace production || true
-sleep 10
-kubectl create namespace production
-
-helm delete --purge jenkins || true
+helm delete --purge devops_jenkins || true
 sleep 10
 
-helm install --name jenkins -f ./config/jenkins.yml stable/jenkins --namespace cd-pipeline
-kubectl create secret generic jenkins-maven-settings --from-file=./settings.xml -n cd-pipeline
-
-kubectl create secret docker-registry regsecret --docker-server=quay.io/cd_pipeline/cloud-repository --docker-username=cd-pipeline --docker-password=$forsythe17@! --docker-email=akalinovski@forsythe.com -n cd-pipeline
+helm install --name devops_jenkins -f ./jenkins.yml stable/jenkins --namespace devops
+kubectl create secret generic jenkins-maven-settings --from-file=./settings.xml -n devops
